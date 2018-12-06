7e34dad581f30f67ca2f53d2d2a22cc91c4d9275
Bernie Cabase <cabasebernie91@gmail.com>
2018-12-06 09:04:15 +0800

    added error 404. update subjects.php

--- application/controllers/Subjects.php
+++ application/controllers/Subjects.php
@@ -8,7 +8,21 @@ class Subjects extends CI_Controller {
 		parent::__construct();
 		$this->load->helper('url');
 		$this->load->model('subjects_model');
+		$this->load->library('user_agent');
 		$this->load->helper('date');
+	}
+	
+	private function view($view, $data = "", $single = false){
+		if ($single) {
+			$this->load->view('includes/head');
+			$this->load->view($view, $data);
+		}else{
+			$this->load->view('includes/head');
+			$this->load->view('includes/top-navigation');
+			$this->load->view('includes/left-navigation');
+			$this->load->view($view, $data);
+			$this->load->view('includes/footer');
+		}
 	}
 	public function index()
 	{
@@ -28,15 +42,21 @@ class Subjects extends CI_Controller {
 	}
 
 	public function add(){
-		$data = array(
-			'subject_title' => $this->input->post('subj-name'),
-			'create_on' => date('Y-d-m')
-		);
-		// if (condition) {
-		// 	# code...
-		// }
-		$add = $this->subjects_model->add($data);
-		echo json_encode(array("status" => TRUE));
+		$subj_name = $this->input->post('subj-name');
+		if (empty($subj_name) || is_null($subj_name)) {
+			$data = array(
+				'heading'=>"Sorry, you are not allowed",
+				 'message'=> "<p>This is a restricted area <a href='#' onclick='history.go(-1);' class=''>Go Back</a></p>");
+			$this->view('error_404', $data , true);
+		}else{
+		
+			$data = array(
+				'subject_title' => $this->input->post('subj-name'),
+				'create_on' => date('Y-d-m')
+			);
+			$add = $this->subjects_model->add($data);
+			echo json_encode(array("status" => TRUE));
+		}
 	}
 
 	public function delete($id){
